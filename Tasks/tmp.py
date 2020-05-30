a57d9a67eead6b8b80eaefdebfba7cd9c3d42991
aeksei <aeksei94@gmail.com>
2020-05-30 12:13:21 +0300

    b1 done

---
+++ Tasks/tmp.py
@@ -0,0 +1,12 @@
+def search_elem(elem, arr) -> bool:
+    """
+    Функция для поиска элемента в последовательности
+
+    :param elem: Что ищем
+    :param arr: Где ищем
+    :return: Bool
+    """
+
+    if elem in arr:
+        return True
+    return False
